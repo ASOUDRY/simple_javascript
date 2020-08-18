0fb7c36ba5dbe1194db3e1bad323a5f85c9bc73c
Alexander <soudryalex@gmail.com>
2020-08-18 14:29:49 -0400

    1.12 is functional

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
-alert("Hello World");
-var favoriteFood = "Frog";
-// document.write works with let and Const too
-document.write(favoriteFood);
+// alert("Hello World");
+// var favoriteFood = "Frog";
+// // document.write works with let and Const too
+// document.write(favoriteFood);
+var pokemonList = [
+{
+	dexNumber: "#" + 037,
+	name: "Ninetales",
+	types: ["fire", "monotype"],
+	height: 1.1 + "m",
+	catchRate: 75,
+	homeRegion: "Kanto"
+},
+{
+	dexNumber: "#" + 066,
+	name: "Machop",
+	types: ["fighting", "monotype"],
+	height: 0.8 + "m",
+	catchRate: 180,
+	homeRegion: "Kanto"
+},
+{
+	dexNumber: "#" + 386,
+	name: "Deoxys",
+	type: ["psychic", "monotype"],
+	height: 1.7 + "m",
+	catchRate: 3,
+	homeregion: "Hoenn"
+}
+];
